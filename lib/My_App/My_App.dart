16cb9b53d57dd45f7dd18b10bbd2129b834052d9
AyaSedik <ayasedik67@gamil.com>
2024-10-10 06:39:15 +0300

    +Splahe +QuranTap +QuranDetails

--- lib/My_App/My_App.dart
+++ lib/My_App/My_App.dart
@@ -6,7 +6,7 @@ import 'package:islame_project_session_6/Presention/Moduels/Home_Moudel/Screens/
 import 'package:islame_project_session_6/config/Theme/My_Theme.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+ const MyApp({super.key});
 
   Widget build(BuildContext context) {
     return MaterialApp(
