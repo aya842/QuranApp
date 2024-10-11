12b2fd09b0560be018ac3e54b6c22d1e98663fab
AyaSedik <ayasedik67@gamil.com>
2024-10-11 03:31:27 +0300

    hadith tap

---
+++ lib/Presention/Moduels/Home_Moudel/Screens/Home/tabs/Hadith/HadithWidget/HadithHeader.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+class  HadithHeader  extends StatelessWidget {
+  const HadithHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding:EdgeInsets.symmetric(vertical: 8) ,
+      width: double.infinity,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        border: Border.symmetric(
+          horizontal: BorderSide(color: Theme.of(context).dividerColor)
+
+        )
+      ),
+      child: Text('Al:AHadith:',style: Theme.of(context).textTheme.bodyLarge,),
+
+    );
+  }
+}
